c37135a5fc0d6ab97f37f91e169b22d01a8b74c3
Alexandr Katyshev <katyshev1@gmail.com>
2022-02-22 00:44:13 +0200

    night checkpoint

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import './js/mobile-menu';
 import './js/modal';
 import './js/buttons';
 import './js/animation-counter';
+import './js/herobg';
+import './js/modal2';
---
+++ src/js/herobg.js
@@ -0,0 +1,41 @@
+// Some random colors
+const colors = ["#E6CFC1", "#F9DE73", "#E98525", "#C7E7BC", "#FFD293", "#F24D7D"];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.querySelector('.hero').append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
