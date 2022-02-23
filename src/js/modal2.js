57f5e92fc61d6bcccaaaf37e5d1ca8803b8744e8
Yevhen <krenichniy1995@gmail.com>
2022-02-23 15:29:25 +0200

    add changes

--- src/js/modal2.js
+++ src/js/modal2.js
@@ -6,7 +6,7 @@
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
